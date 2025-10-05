669007f989209c4a2d75dd94ad14905887d24104
Ayush Nafdey <nafdeyayush@gmail.com>
2025-10-05 15:59:15 +0530

    Minor changes

--- app/src/firebase.js
+++ app/src/firebase.js
@@ -2,8 +2,8 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  apiKey: import.meta.env.VITE_API,
+  authDomain: import.meta.env.VITE_DOMAIN,
   databaseURL: import.meta.env.VITE_DATABASE_URL,
   projectId: import.meta.env.VITE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
